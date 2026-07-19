7ce26eff23180c0d1564414dc062ee1eeb5cdee0
Александр <sasha-kvasnikov@mail.ru>
2026-07-19 14:18:16 +0300

    feat: add `CollectIds` trait for bulk fetching

---
+++ crates/db/src/core/bulk.rs
@@ -0,0 +1,31 @@
+use models::{
+    entities::{Asset, AssetFeatures, MediaFile},
+    types::{AssetId, MediaId},
+};
+
+pub trait CollectIds<T> {
+    fn ids(&self) -> Vec<T>;
+}
+
+impl CollectIds<AssetId> for Vec<Asset> {
+    fn ids(&self) -> Vec<AssetId> {
+        self.iter().map(|a| a.id).collect()
+    }
+}
+impl CollectIds<MediaId> for Vec<Asset> {
+    fn ids(&self) -> Vec<MediaId> {
+        self.iter().map(|a| a.media_id.clone()).collect()
+    }
+}
+
+impl CollectIds<MediaId> for Vec<MediaFile> {
+    fn ids(&self) -> Vec<MediaId> {
+        self.iter().map(|m| m.media_id.clone()).collect()
+    }
+}
+
+impl CollectIds<AssetId> for Vec<AssetFeatures> {
+    fn ids(&self) -> Vec<AssetId> {
+        self.iter().map(|a| a.asset_id).collect()
+    }
+}
--- crates/db/src/core/mod.rs
+++ crates/db/src/core/mod.rs
@@ -1,3 +1,4 @@
+pub mod bulk;
 pub mod pagination;
 pub mod patches;
 pub mod result;
