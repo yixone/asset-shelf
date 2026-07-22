8e38ed46063e475265e48ede83e152c94e4238f3
Александр <sasha-kvasnikov@mail.ru>
2026-07-22 20:55:12 +0300

    remove utoipa

--- crates/server/src/routes/mod.rs
+++ crates/server/src/routes/mod.rs
@@ -1,30 +1,12 @@
-use actix_web::{HttpResponse, get, web};
+use actix_web::{HttpResponse, web};
 use result::Result;
-use utoipa::OpenApi;
-use utoipa_swagger_ui::SwaggerUi;
+
+use crate::SERVER_VERSION;
 
 pub mod v1;
 
 type ApiResult = Result<HttpResponse>;
 
-#[derive(utoipa::OpenApi)]
-#[openapi(info(title = "Asset shelf API"), paths(ping))]
-pub struct ApiDoc;
-
 pub fn cfg(cfg: &mut web::ServiceConfig) {
-    let docs = ApiDoc::openapi();
-
-    cfg.service(ping);
-    cfg.service(SwaggerUi::new("/docs/{_:.*}").url("/openapi.json", docs));
-}
-
-#[derive(utoipa::ToSchema, serde::Serialize)]
-struct PingResponse {
-    message: &'static str,
-}
-
-#[utoipa::path(get, path = "/ping", responses((status = 200, body = PingResponse)))]
-#[get("/ping")]
-async fn ping() -> ApiResult {
-    Ok(HttpResponse::Ok().json(PingResponse { message: "pong" }))
+    cfg.configure(v1::cfg);
 }
