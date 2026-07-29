97161bb9f54ee73b3dc45fd03bcb6989a673ade4
Александр <sasha-kvasnikov@mail.ru>
2026-07-29 12:20:05 +0300

    feat: impl collections repository

--- crates/db/migrations/20260728160647_collections.sql
+++ crates/db/migrations/20260728160647_collections.sql
@@ -14,16 +14,16 @@ CREATE INDEX coll_name_idx ON collections(name);
 CREATE TABLE collection_assets (
     id              BIGINT          NOT NULL,
 
-    collection      BIGINT          NOT NULL,
-    asset           BIGINT          NOT NULL,
+    collection_id   BIGINT          NOT NULL,
+    asset_id        BIGINT          NOT NULL,
 
     added_at        TIMESTAMPTZ     NOT NULL,
 
     PRIMARY KEY (id),
 
-    FOREIGN KEY (collection) REFERENCES collections(id)  ON DELETE CASCADE,
-    FOREIGN KEY (asset)      REFERENCES assets(id)       ON DELETE CASCADE
+    FOREIGN KEY (collection_id) REFERENCES collections(id)  ON DELETE CASCADE,
+    FOREIGN KEY (asset_id)      REFERENCES assets(id)       ON DELETE CASCADE
 );
 
-CREATE INDEX coll_assets_asset_idx  ON collection_assets(asset);
-CREATE INDEX coll_assets_coll_idx   ON collection_assets(collection);
+CREATE INDEX coll_assets_asset_idx  ON collection_assets(asset_id);
+CREATE INDEX coll_assets_coll_idx   ON collection_assets(collection_id);
--- crates/db/src/queries/collections.rs
+++ crates/db/src/queries/collections.rs
@@ -1,9 +1,254 @@
 use flake_id::str::FlakeIdStr;
 use models::{
-    entities::CollectionAdditions,
-    types::{CollectionId, MediaId},
+    entities::{Collection, CollectionAdditions, CollectionAsset},
+    types::{AssetsOrdering, CollectionAssetId, CollectionId, MediaId},
 };
-use sqlx::prelude::FromRow;
+use result::{Result, error::ResultExt};
+use sqlx::{QueryBuilder, prelude::FromRow};
+
+use crate::{
+    ops::{CollectionAssetsOps, CollectionsOps},
+    sqlite::SqliteExecutor,
+    types::{DeleteResult, InsertResult, Pagination, UpdateResult, patches::CollectionPatch},
+};
+
+impl<T> CollectionsOps for T
+where
+    T: SqliteExecutor,
+{
+    async fn insert_collection(&mut self, c: &Collection) -> Result<InsertResult> {
+        let res = sqlx::query(
+            "
+            INSERT INTO collections (
+                id,
+                name, description,
+                created_at
+            )
+            VALUES (
+                ?,
+                ?, ?,
+                ?
+            )
+            ",
+        )
+        .bind(c.id)
+        .bind(&c.name)
+        .bind(&c.description)
+        .bind(c.created_at)
+        .execute(self.executor())
+        .await
+        .to_app_err()?;
+        Ok(res.into())
+    }
+
+    async fn update_collection(
+        &mut self,
+        id: CollectionId,
+        patch: CollectionPatch,
+    ) -> Result<UpdateResult<Collection>> {
+        let mut qb = QueryBuilder::new(
+            "
+            UPDATE collections
+            SET
+            ",
+        );
+
+        patch.apply_sql(&mut qb);
+
+        qb.push(" WHERE id = ");
+        qb.push_bind(id);
+
+        qb.push(" RETURNING * ");
+
+        let res = qb
+            .build_query_as()
+            .fetch_optional(self.executor())
+            .await
+            .to_app_err()?;
+        Ok(res.into())
+    }
+
+    async fn delete_collection(&mut self, id: CollectionId) -> Result<DeleteResult> {
+        let res = sqlx::query(
+            "
+            DELETE FROM collections
+            WHERE id = ?
+            ",
+        )
+        .bind(id)
+        .execute(self.executor())
+        .await
+        .to_app_err()?;
+        Ok(res.into())
+    }
+
+    async fn list_collections(&mut self, p: Pagination) -> Result<Vec<Collection>> {
+        if p.limit() == 0 {
+            return Ok(Vec::new());
+        }
+
+        let mut qb = QueryBuilder::new(
+            "
+            SELECT c.* FROM collections AS c
+            ",
+        );
+        p.apply_sql(&mut qb);
+
+        qb.build_query_as()
+            .fetch_all(self.executor())
+            .await
+            .to_app_err()
+    }
+
+    async fn get_collections_bulk(&mut self, ids: &[CollectionId]) -> Result<Vec<Collection>> {
+        if ids.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        let mut qb = QueryBuilder::new(
+            "
+            SELECT * FROM collections
+            WHERE id IN
+            ",
+        );
+        qb.push_tuples(ids, |mut qb, id| {
+            qb.push_bind(id);
+        });
+
+        qb.build_query_as()
+            .fetch_all(self.executor())
+            .await
+            .to_app_err()
+    }
+
+    async fn get_collections_additions_bulk(
+        &mut self,
+        ids: &[CollectionId],
+    ) -> Result<Vec<CollectionAdditions>> {
+        if ids.is_empty() {
+            return Ok(Vec::new());
+        }
+
+        let mut qb = QueryBuilder::new(
+            "
+            SELECT 
+            c.id,
+            (
+                SELECT group_concat(media_id, ';')
+                FROM (
+                    SELECT a.media_id
+                    FROM collection_assets AS ca
+                    JOIN assets AS a 
+                        ON a.id = ca.asset_id
+                    WHERE ca.collection_id = c.id AND a.deleted_at IS NULL
+                    ORDER BY ca.added_at DESC
+                    LIMIT 3 OFFSET 0
+                )
+            ) AS thumbnails,
+            (
+                SELECT COUNT(a.id)
+                FROM collection_assets AS ca
+                LEFT JOIN assets AS a ON a.id = ca.asset_id
+                WHERE a.deleted_at IS NULL AND ca.collection_id = c.id
+            ) AS assets_count 
+            FROM collections AS c
+            WHERE c.id IN
+            ",
+        );
+        qb.push_tuples(ids, |mut qb, id| {
+            qb.push_bind(id);
+        });
+
+        let additions: Vec<CollectionAdditionsRow> = qb
+            .build_query_as()
+            .fetch_all(self.executor())
+            .await
+            .to_app_err()?;
+
+        Ok(additions
+            .into_iter()
+            .map(CollectionAdditions::from)
+            .collect())
+    }
+}
+
+impl<T> CollectionAssetsOps for T
+where
+    T: SqliteExecutor,
+{
+    async fn insert_collection_asset(&mut self, ca: &CollectionAsset) -> Result<InsertResult> {
+        let res = sqlx::query(
+            "
+            INSERT INTO collection_assets (
+                id, collection_id, asset_id, added_at
+            )
+            VALUES (
+                ?, ?, ?, ?
+            )
+            ",
+        )
+        .bind(ca.id)
+        .bind(ca.collection_id)
+        .bind(ca.asset_id)
+        .bind(ca.asset_id)
+        .execute(self.executor())
+        .await
+        .to_app_err()?;
+        Ok(res.into())
+    }
+
+    async fn remove_collection_asset(&mut self, id: &CollectionAssetId) -> Result<DeleteResult> {
+        let res = sqlx::query(
+            "
+            DELETE FROM collection_assets
+            WHERE id = ?
+            ",
+        )
+        .bind(id)
+        .execute(self.executor())
+        .await
+        .to_app_err()?;
+        Ok(res.into())
+    }
+
+    async fn get_collection_assets(
+        &mut self,
+        id: &CollectionId,
+        p: Pagination,
+        o: AssetsOrdering,
+    ) -> Result<Vec<CollectionAsset>> {
+        if p.limit() == 0 {
+            return Ok(Vec::new());
+        }
+
+        let mut qb = QueryBuilder::new(
+            "
+            SELECT ca.* 
+            FROM collections AS c
+            JOIN collection_assets AS ca
+                ON ca.collection_id = c.id
+            JOIN assets AS a
+                ON a.id = ca.asset_id
+            WHERE a.deleted_at IS NULL
+            ",
+        );
+        let mut query = qb.separated(" ");
+
+        query.push("AND c.id =");
+        query.push_bind(id);
+
+        match o {
+            AssetsOrdering::Newest => query.push("ORDER BY ca.added_at DESC"),
+            AssetsOrdering::Oldest => query.push("ORDER BY ca.added_at ASC"),
+        };
+        p.apply_sql(&mut qb);
+
+        qb.build_query_as()
+            .fetch_all(self.executor())
+            .await
+            .to_app_err()
+    }
+}
 
 /// Raw database row for mapping in [`CollectionAdditions`]
 #[derive(FromRow)]
