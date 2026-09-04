8d7dd30818dac08d8ae2cc7b1784fce8bde2de95
Александр <sasha-kvasnikov@mail.ru>
2026-09-04 20:35:35 +0300

    feat(db): add generic database entity

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -1,4 +1,36 @@
+use std::sync::Arc;
+
 pub use db_core::repos::RepositoryContext;
 
 pub use db_core::{ops, repos, types};
-pub use db_sqlite as sqlite;
+
+use db_sqlite::SqliteDatabase;
+
+/// Selected database driver
+pub enum Database {
+    /// Sqlite DB driver
+    Sqlite(SqliteDatabase),
+}
+
+impl Database {
+    /// Opens an [`SqliteDatabase`] from the specified file and applies migrations
+    pub async fn sqlite(file: impl AsRef<std::path::Path>) -> result::Result<Self> {
+        let db = SqliteDatabase::open(file).await?;
+        db.migrate().await?;
+
+        Ok(Database::Sqlite(db))
+    }
+
+    /// Creates a [`RepositoryContext`] using the current [`Database`]
+    pub fn repos(self) -> RepositoryContext {
+        match self {
+            Database::Sqlite(db) => db.repositories(),
+        }
+    }
+
+    /// Creates a [`RepositoryContext`] using the current [`Database`]
+    /// and wraps this context in an [`Arc`] for sharing between threads
+    pub fn repos_shared(self) -> Arc<RepositoryContext> {
+        Arc::new(self.repos())
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,7 +3,7 @@ use std::{sync::Arc, time::Duration};
 use actix_cors::Cors;
 use actix_web::{App, HttpServer, dev::ServerHandle, web};
 use config::{DatabaseDriverConfig, StorageBackendConfig};
-use db::sqlite::SqliteDatabase;
+use db::Database;
 use events::EventBus;
 use flake_id::FlakeIdGenerator;
 use jobs::{Job, JobContext};
@@ -21,29 +21,34 @@ use tokio_util::sync::CancellationToken;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Initializes the tracer and outputs basic server information
     init_tracing();
     tracing::info!(version = SERVER_VERSION, "Starting the server:");
 
+    // Loads the application configuration.
     let cfg = load_config()?;
 
+    // Registers application metrics (if enabled)
     let metrics_reg = MetricsRegistry::new(cfg.instance.telemetry.enabled());
     let metrics_ctx = MetricsCtx::try_new(metrics_reg)?;
     if cfg.instance.telemetry.enabled() {
         tracing::info!("Telemetry is enabled");
     }
 
+    // Creates a shared ID generator
     let flake = Arc::new(FlakeIdGenerator::new(cfg.instance.node_id()));
 
+    // Opens the database
     let db = match cfg.database.driver() {
         DatabaseDriverConfig::Sqlite { path } => {
-            let db = SqliteDatabase::open(path).await?;
+            let db = Database::sqlite(path).await?;
             tracing::info!("Opening SQLite from file: {path}");
-            db.migrate().await?;
-
-            Arc::new(db.repositories())
+            db
         }
     };
+    let repos = db.repos_shared();
 
+    // Opens the file storage
     let storage = match cfg.storage.backend() {
         StorageBackendConfig::Native { dir, temp } => {
             let storage_backend = NativeFsStorageBackend::new(dir).await?;
@@ -53,17 +58,20 @@ async fn main() -> Result<()> {
         }
     };
 
+    // Creates a cancellation token
     let cancel = CancellationToken::new();
 
+    // Initializes the event bus
     tracing::info!("Initializing event bus");
     let events = Arc::new(EventBus::new(1024));
 
     tracing::info!("Initializing background jobs");
 
+    // Initializes the background job resolver
     let resolver = JobsResolver::<Job>::builder()
         .dispatcher(JobsDispatcher::new())
         .context(JobContext {
-            db: db.clone(),
+            db: repos.clone(),
             storage: storage.clone(),
             flake: flake.clone(),
         })
@@ -73,6 +81,7 @@ async fn main() -> Result<()> {
     tracing::info!("Jobs resolver created");
     tracing::info!("Set workers count: {}", resolver.workers_count());
 
+    // Schedules background jobs
     resolver.schedule(
         Job::CleanupStorageMedia,
         JobSchedule::interval(Duration::from_mins(30)),
@@ -83,11 +92,13 @@ async fn main() -> Result<()> {
     );
     tracing::info!("Jobs have been added to the scheduler");
 
+    // Launches background workers
     let jobs_resolver_handle = resolver.run();
     tracing::info!("Jobs resolver runned!");
 
+    // Creates the application context
     let ctx = DataCtx {
-        db,
+        db: repos,
         storage,
         flake: flake.clone(),
         jobs: resolver,
@@ -95,11 +106,14 @@ async fn main() -> Result<()> {
         config: cfg.clone(),
     };
 
+    // Configures the server
     let server = configure_server(ctx, metrics_ctx, &cfg.server.listen_addr())?;
     let handle = server.handle();
 
+    // Spawns graceful shutdown handler
     spawn_shutdown_handler(cancel, handle, jobs_resolver_handle);
 
+    // Starts the server
     tracing::info!("Server configurated");
     tracing::info!("Server started on http://{}!", cfg.server.listen_addr());
     server.await.to_app_err()?;
